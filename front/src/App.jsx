179753152e81a2f181334ad3d4d5338b79d7ec2f
Asif <masifmirza927@gmail.com>
2024-07-02 16:43:48 +0500

    data table

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,45 +6,86 @@ import DataTable from 'react-data-table-component';
 function App() {
   const [contacts, setContacts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [formdata, setFormdata] = useState({
+    id: null,
+    name: "",
+    city: "",
+    age: ""
+  });
 
-  const handleEdit = (row) => {
-    console.log(row);
-
-  }
-
-  
   const columns = [
     {
       name: 'Name',
       selector: row => row.name,
-      sortable: true,
-      // cell: row => <div><h3 className='text-2xl text-red-400'>{row.name}</h3></div>
     },
     {
       name: 'City',
       selector: row => row.city,
     },
     {
-      name: 'Actions',
-      cell: row => (
-        <div className='flex w-20'>
-          <button className="btn primary btn-sm" onClick={() => handleEdit(row)}>Edit</button>
-          <button className="btn danger btn-sm" onClick={() => handleDelete(row)} style={{ marginLeft: 8 }}>Delete</button>
-        </div>
-      ),
-      // ignoreRowClick: true,
-      // allowOverflow: true,
-      // button: true,
+      name: 'Age',
+      selector: row => row.age,
     },
+    {
+      name: 'Actions',
+      cell: (row) => (
+        <div className='flex w-20'>
+          <button className="btn primary btn-sm btn-info" onClick={() => handleEditBtn(row)} >Edit</button>
+          <button className="btn danger btn-sm btn-error" style={{ marginLeft: 8 }}>Delete</button>
+        </div>
+      )
+    }
+
   ];
 
-  useEffect(() => {
-    setLoading(true);
+
+  // hanle edit button
+  const handleEditBtn = (row) => {
+    console.log(row);
+    setFormdata( {
+      id: row._id,
+      name: row.name,
+      city: row.city,
+      age: row.age
+    });
+    document.getElementById('my_modal_1').showModal();
+  }
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFormdata((prev) => {
+      return ({
+        ...prev,
+        [name]: value
+      })
+    });
+  }
+
+const fetchData = () => {
+  setLoading(true);
     axios.get("http://localhost:3001/contacts").then((res) => {
       setContacts(res.data.contacts);
     }).catch((err) => {
 
     }).finally(() => { setLoading(false) })
+}
+
+// handle update api call
+const handleUpdateApi = () => {
+  
+  setLoading(true);
+
+    axios.put(`http://localhost:3001/contacts/${formdata.id}`, formdata).then( (res) => {
+      console.log("successfully updated");
+      fetchData();
+      document.getElementById('my_modal_1').close();
+      
+    }).catch(err => console.log(err.message))
+    .finally( () => setLoading(false))
+}
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
 
@@ -56,37 +97,46 @@ function App() {
           <DataTable
             columns={columns}
             data={contacts}
-            noRowsPerPage={5}
-            pagination={true}
+            pagination
           />
           {
             loading && <div className='text-center'><span className="loading loading-dots loading-lg text-error"></span></div>
           }
-          {/* <table class="table-auto w-full border">
-            <thead>
-              <tr className='bg-blue-400'>
-                <th className='text-center p-3'>Name</th>
-                <th className='text-center p-3'>City</th>
-                <th className='text-center p-3'>Age</th>
-              </tr>
-            </thead>
-            <tbody>
 
-              {
-                contacts.map((contact, index) => {
-                  return (<>
-                    <tr className='odd:bg-white even:bg-slate-100'>
-                      <td className='text-center p-3'>{contact.name}</td>
-                      <td className='text-center p-3'>{contact.city}</td>
-                      <td className='text-center p-3'>{contact.age}</td>
-                    </tr>
-                  </>)
-                })
-              }
-            </tbody>
-          </table> */}
         </div>
       </div>
+
+
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg mb-5">Update Contact!</h3>
+
+          <div>
+            <input type="text" value={formdata.name} onChange={handleOnChange} name='name' placeholder="name" className="mb-4 input input-bordered w-full max-w-xs" />
+            <input type="text" value={formdata.city} onChange={handleOnChange} name='city' placeholder="city" className="mb-4 input input-bordered w-full max-w-xs" />
+            <input type="text" value={formdata.age} onChange={handleOnChange} name='age' placeholder="age" className="mb-4 input input-bordered w-full max-w-xs" />
+
+          </div>
+
+          <div className="modal-action">
+            <button className="btn" onClick={ handleUpdateApi } disabled={loading}>
+              {loading && <span className="loading loading-spinner text-error loading-sm"></span>}
+              Update
+              </button>
+
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+
+
+        </div>
+      </dialog>
+
+
     </>
   )
 }
